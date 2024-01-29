4608c66ded1e9465923ac19d40316f7bd698116b
AdeyinkaEsi1 <yhamid2828@gmail.com>
2024-01-29 02:32:31 +0100

    added an error message for form filing

--- TextChecker/views.py
+++ TextChecker/views.py
@@ -11,8 +11,12 @@ def Text(request):
             form = SignUpForm(request.POST or None)
             if form.is_valid():
                   form.save()
-            messages.success(request, ('form submitted successfully'))
-            return render(request, 'TextChecker\Thanks.html', {'all': all_users,})
+                  messages.success(request, ('form submitted successfully'))
+                  return render(request, 'TextChecker\Thanks.html', {'all': all_users,})
+            else:
+                  messages.success(request, 'error in form')
+                  return render(request, 'TextChecker\home.html', {'all': all_users,})
+            
       else:
           form = SignUpForm()
       return render(request, 'TextChecker\home.html', {})
